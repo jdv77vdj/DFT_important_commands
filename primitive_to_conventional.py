f9ffaf96e7f7891b41455e867b7868e6ea75dece
jdv77vdj <107513871+jdv77vdj@users.noreply.github.com>
2023-11-01 14:45:19 -0500

    Create primitive_to_conventional.py

---
+++ primitive_to_conventional.py
@@ -0,0 +1,16 @@
+# Primitive cell to conventional cell
+
+from pymatgen.core import Lattice, Structure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+import pymatgen.io.vasp
+structure = Structure.from_file("POSCAR")
+sga = SpacegroupAnalyzer(structure)
+sga.get_conventional_standard_structure()
+conventional_structure = sga.get_conventional_standard_structure()
+
+# Write file
+# In [path], insert path to write the POSCAR file of conventional cell
+pymatgen.io.vasp.Poscar.write_file(pymatgen.io.vasp.Poscar(
+conventional_structure),’[path]/conventional_cell.POSCAR’)
+
+
