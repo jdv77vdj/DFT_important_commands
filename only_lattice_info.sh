46ca9c4ebfb554195f67eee5258879af0d83ccaa
jdv77vdj <107513871+jdv77vdj@users.noreply.github.com>
2023-12-08 12:52:06 -0600

    Update only_lattice_info.sh

--- only_lattice_info.sh
+++ only_lattice_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This file is for printing only the first lines of  without all the atoms positions
-output=$(python lattice_parameters.py)
+#This file is for printing only the first lines of 'get_lattice_parameters.py' without all the atoms positions
+output=$(python get_lattice_parameters.py)
 # Print the first 9 lines of the output
 echo "$output" | head -n 9
