fa87016f3dfc831c782db4c3100fb3b7b726c7f6
jdv77vdj <107513871+jdv77vdj@users.noreply.github.com>
2023-12-08 13:06:03 -0600

    get_lattice_parameters.py

--- get_lattice_parameters.py
+++ get_lattice_parameters.py
@@ -1,6 +1,19 @@
+import sys
 from pymatgen.core import Lattice, Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer 
 import pymatgen.io.vasp
+
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python lattice_parameters.py name_of_file(CONTCAR, POSCAR, etc)")
+    sys.exit(1)
+
+file_name = sys.argv[1] 
+try:
+    structure = Structure.from_file(file_name)
+except Exception as e:
+    print("Error:", e)
+    sys.exit(1)
 
 structure = Structure.from_file("CONTCAR")
 sga = SpacegroupAnalyzer(structure)
