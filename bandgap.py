aff459e0217fe82fddcb11009cc93cf3238f679a
jdv77vdj <107513871+jdv77vdj@users.noreply.github.com>
2023-12-13 19:14:48 -0600

    Create bandgap.py

---
+++ bandgap.py
@@ -0,0 +1,6 @@
+# Get the band gap of a DOS calculation with VASP
+#EIGENVAL file is needed
+
+import pymatgen.io.vasp.outputs as pmg
+eig = pmg.Eigenval('EIGENVAL')
+print('Band gap: ', eig.eigenvalue_band_properties[0])
