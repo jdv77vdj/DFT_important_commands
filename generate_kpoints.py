b7f19e1068d70a31e583ba106b4cec30ed27bc4c
jdv77vdj <107513871+jdv77vdj@users.noreply.github.com>
2024-01-30 14:19:07 -0600

    Update generate_kpoints.py

--- generate_kpoints.py
+++ generate_kpoints.py
@@ -4,8 +4,8 @@ from pymatgen.io.vasp.inputs import Kpoints
 
 # Read the structure from the POSCAR file
 structure = Structure.from_file("POSCAR")
-Kpoints.automatic_density(structure,kppa=1000)
-
+kpoints = Kpoints.automatic_density(structure,kppa=1000)
 #kppa is the grid density
+kpoints.write_file("KPOINTS")
 
-# For understanding the documentation, read: https://pymatgen.org/pymatgen.io.vasp.html
+# For understanding the meaning of commands and variables, read: https://pymatgen.org/pymatgen.io.vasp.html
